1eaa9f4432d0f952c7f97f032011755fcbb9136f
meerakmr <meerakmr@umich.edu>
2022-12-04 17:34:07 -0500

    Clickable images I put descriptions in for each album and made the images clickable.

---
+++ js/node.js
@@ -0,0 +1,131 @@
+// when you click on an album cover, description text will appear
+document.querySelector("#airplanes").addEventListener("click", function() {
+    console.log("airplanes");
+    let desc = document.querySelector("#airplanes p");
+    let img = document.querySelector("#airplanes img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#facts").addEventListener("click", function() {
+    console.log("facts");
+    let desc = document.querySelector("#facts p");
+    let img = document.querySelector("#facts img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#photoalbum").addEventListener("click", function() {
+    console.log("photoalbum");
+    let desc = document.querySelector("#photoalbum p");
+    let img = document.querySelector("#photoalbum img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#transat").addEventListener("click", function() {
+    console.log("transat");
+    let desc = document.querySelector("#transat p");
+    let img = document.querySelector("#transat img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#plans").addEventListener("click", function() {
+    console.log("plans");
+    let desc = document.querySelector("#plans p");
+    let img = document.querySelector("#plans img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#stairs").addEventListener("click", function() {
+    console.log("stairs");
+    let desc = document.querySelector("#stairs p");
+    let img = document.querySelector("#stairs img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#codes").addEventListener("click", function() {
+    console.log("codes");
+    let desc = document.querySelector("#codes p");
+    let img = document.querySelector("#codes img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#kintsugi").addEventListener("click", function() {
+    console.log("kintsugi");
+    let desc = document.querySelector("#kintsugi p");
+    let img = document.querySelector("#kintsugi img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#tyft").addEventListener("click", function() {
+    console.log("tyft");
+    let desc = document.querySelector("#tyft p");
+    let img = document.querySelector("#tyft img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
+document.querySelector("#asphalt").addEventListener("click", function() {
+    console.log("asphalt");
+    let desc = document.querySelector("#asphalt p");
+    let img = document.querySelector("#asphalt img");
+    if (desc.style.display == "block") {
+        desc.style.display = "none";
+        img.style.display = "block";
+    }
+    else {
+        desc.style.display = "block";
+        img.style.display = "none";
+    }
+})
