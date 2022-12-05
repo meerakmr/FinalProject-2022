4be428776bad6b2fe5bdd9a6e8a79012a044b2e2
meerakmr <meerakmr@umich.edu>
2022-12-05 15:26:52 -0500

    fix all WAVE errors

--- js/node3.js
+++ js/node3.js
@@ -12,38 +12,38 @@ function displayDesc(desc, img) {
 }
 document.querySelector("#creatures").addEventListener("click", function() {
     console.log("creatures");
-    displayDesc(document.querySelector("#creatures h3"), document.querySelector("#creatures img"));
+    displayDesc(document.querySelector("#creatures p"), document.querySelector("#creatures img"));
 })
 document.querySelector("#infamous").addEventListener("click", function() {
     console.log("infamous");
-    displayDesc(document.querySelector("#infamous h3"), document.querySelector("#infamous img"));
+    displayDesc(document.querySelector("#infamous p"), document.querySelector("#infamous img"));
 })
 document.querySelector("#reincarnate").addEventListener("click", function() {
     console.log("reincarnate");
-    displayDesc(document.querySelector("#reincarnate h3"), document.querySelector("#reincarnate img"));
+    displayDesc(document.querySelector("#reincarnate p"), document.querySelector("#reincarnate img"));
 })
 document.querySelector("#graveshift").addEventListener("click", function() {
     console.log("graveyard shift");
-    displayDesc(document.querySelector("#graveshift h3"), document.querySelector("#graveshift img"));
+    displayDesc(document.querySelector("#graveshift p"), document.querySelector("#graveshift img"));
 })
 document.querySelector("#disguise").addEventListener("click", function() {
     console.log("disguise");
-    displayDesc(document.querySelector("#disguise h3"), document.querySelector("#disguise img"));
+    displayDesc(document.querySelector("#disguise p"), document.querySelector("#disguise img"));
 })
 document.querySelector("#steotw").addEventListener("click", function() {
     console.log("steotw");
-    displayDesc(document.querySelector("#steotw h3"), document.querySelector("#steotw img"));
+    displayDesc(document.querySelector("#steotw p"), document.querySelector("#steotw img"));
 })
 
 document.querySelector("#etitb").addEventListener("click", function() {
     console.log("etitb");
-    displayDesc(document.querySelector("#etitb h3"), document.querySelector("#etitb img"));
+    displayDesc(document.querySelector("#etitb p"), document.querySelector("#etitb img"));
 })
 document.querySelector("#silverscream").addEventListener("click", function() {
     console.log("silver scream");
-    displayDesc(document.querySelector("#silverscream h3"), document.querySelector("#silverscream img"));
+    displayDesc(document.querySelector("#silverscream p"), document.querySelector("#silverscream img"));
 })
 document.querySelector("#horrorwood").addEventListener("click", function() {
     console.log("horrorwood");
-    displayDesc(document.querySelector("#horrorwood h3"), document.querySelector("#horrorwood img"));
+    displayDesc(document.querySelector("#horrorwood p"), document.querySelector("#horrorwood img"));
 })
